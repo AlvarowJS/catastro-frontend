ba32fb7c78aa5499469815a887f5f5b667907457
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2023-07-31 12:49:54 -0500

    primer commit

---
+++ src/navigation/horizontal/index.js
@@ -0,0 +1,27 @@
+import { Mail, Home, Upload, Eye, Folder, CreditCard, Users, MessageSquare, File, User, MessageCircle } from "react-feather";
+
+const filteredMenuItems = (role) => {
+  const menuItems = [
+    {
+      id: "fichas",
+      title: "Fichas",
+      icon: <Home size={20} />,
+      navLink: "/fichas",
+    },
+    {
+      id: "users",
+      title: "Personal",
+      icon: <Users size={20} />,
+      navLink: "/personal",
+    },
+ 
+  ];
+
+  // const role = localStorage?.getItem("role");
+  
+  return role === "2" ? menuItems?.filter(item => item.id === "inventario" || item.id === "clientes") : menuItems;
+}
+
+
+export default filteredMenuItems;
+
